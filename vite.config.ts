e2482510bdde7eddf4665f58d2b899d19c194bd1
anxgls <230103016@gmail.com>
2024-07-25 14:46:12 +0500

    comm

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === 'production' ? '/Tech-Store/' : '/',
+	base: process.env.NODE_ENV === 'production' ? 'Tech-Store' : '/',
+	build: {
+		outDir: 'dist',
+	},
 	plugins: [react()],
 })
