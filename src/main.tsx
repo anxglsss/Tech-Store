c79d319fe73b1e59758c3ca67b54ebcc85afee31
anxgls <230103016@gmail.com>
2024-07-25 13:54:49 +0500

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 // src/index.tsx
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
@@ -23,7 +22,5 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+	<RouterProvider router={router} />
 )
